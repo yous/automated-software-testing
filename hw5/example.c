42c63d4967f726f8f646a26652e4ffd99e981e26
Chayoung You <yousbe@gmail.com>
2015-11-17 22:14:25 +0900

    Verify `max_heapify` by using CBMC

--- hw5/example.c
+++ hw5/example.c
@@ -1,14 +1,13 @@
-/* Example code */
 #include <stdio.h>
 
-#define MAX 16
-#define H_SIZE 10
+/* The size of x[] is 8 */
+#define MAX 8
 #define parent(i) (i / 2)
 #define left(i) (2 * i)
 #define right(i) (2 * i + 1)
 
 /* Ignore the first 0, since max heap contents start at index 1 */
-int a[MAX] = {0, 16, 4, 10, 14, 7, 9, 3, 2, 8, 1, };
+int a[MAX] = {0, };
 
 void max_heapify(int x[], int i, int h_size) {
   int largest, tmp;
@@ -31,11 +30,73 @@ void max_heapify(int x[], int i, int h_size) {
   }
 }
 
-int main() {
-  int i;
-  max_heapify(a, 2, H_SIZE);
-  for (i = 1; i <= H_SIZE; i++) {
-    printf("%d ", a[i]);
+void max_heap_assume(int x[], int i, int h_size) {
+  int l = left(i);
+  int r = right(i);
+
+  if (l <= h_size) {
+    __CPROVER_assume(x[i] >= x[l]);
+    max_heap_assume(x, l, h_size);
   }
+  if (r <= h_size) {
+    __CPROVER_assume(x[i] >= x[r]);
+    max_heap_assume(x, r, h_size);
+  }
+}
+
+void max_heap_verify(int x[], int i, int h_size) {
+  int l = left(i);
+  int r = right(i);
+
+  if (l <= h_size) {
+    /* The shape property */
+    assert(x[l] > 0);
+    /* The max-heap property */
+    assert(x[i] >= x[l]);
+
+    max_heap_verify(x, l, h_size);
+  }
+  if (r <= h_size) {
+    /* The shape property */
+    assert(x[r] > 0);
+    /* The max-heap property */
+    assert(x[i] >= x[r]);
+
+    max_heap_verify(x, r, h_size);
+  }
+}
+
+void max_heapify_verify(int x[], int i, int h_size) {
+  int l = left(i);
+  int r = right(i);
+
+  /* The right and left sub-trees of node i are max heaps, but that x[i] may be
+     smaller than its children */
+  max_heap_assume(x, left(i), h_size);
+  max_heap_assume(x, right(i), h_size);
+
+  max_heapify(x, i, h_size);
+
+  /* Verify two constraints of max heap */
+  max_heap_verify(x, 1, h_size);
+}
+
+int main() {
+  int i, j;
+  int h_size;
+
+  i = nondet_int();
+  __CPROVER_assume(1 <= i && i < MAX);
+
+  h_size = nondet_int();
+  __CPROVER_assume(1 <= h_size && h_size < MAX);
+
+  for (j = 1; j <= h_size; j++) {
+    a[j] = nondet_int();
+    __CPROVER_assume(a[j] > 0);
+  }
+
+  max_heapify_verify(a, i, h_size);
+
   return 0;
-} /* Output: 16 14 10 8 7 9 3 2 4 1 */
+}
